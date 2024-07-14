e9bf7b0240397865cc645ca7a40c585c114903f5
Transistor Technician <133607593+AceOfSpadesCard@users.noreply.github.com>
2024-07-14 16:35:25 -0700

    Create return_length_of_arguments_passed.ts

---
+++ return_length_of_arguments_passed.ts
@@ -0,0 +1,6 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return (Array.from(arguments)).length;
+    
+};
