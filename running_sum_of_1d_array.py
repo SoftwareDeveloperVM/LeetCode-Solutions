272babd960a4f3559a3da4edeafa675c4847a4f9
Computer Science <133607593+AceOfSpadesCard@users.noreply.github.com>
2024-07-17 08:39:10 -0700

    Update running_sum_of_1d_array.py

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        numer = nums[0]
+        newnums = [numer]
+        nums.pop(0)
+        
+        for number in nums:
+            numer += number
+            newnums.append(numer)
 
+        return newnums
